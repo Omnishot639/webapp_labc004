7c52f0cf4d6924105b7ddfc6f6f13b5a4151545e
Omnishot639 <61055609+Omnishot639@users.noreply.github.com>
2025-06-27 12:00:32 -0300

    Update app.py
    Trying to solve db initialization

--- app.py
+++ app.py
@@ -159,7 +159,6 @@ def deletar_agendamento(id):
     db.session.commit()
     return jsonify({"message": "Agendamento removido"})
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()
+app.run(debug=True)
