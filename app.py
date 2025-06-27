1d1b3d99af69115cd14f4a6803ff73fcba2b76fa
Omnishot639 <61055609+Omnishot639@users.noreply.github.com>
2025-06-27 11:36:29 -0300

    Table additions app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from models import Reagente, Meio, Agendamento
 import os
 
 app = Flask(__name__)
